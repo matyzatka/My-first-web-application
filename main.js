bd1680045a11bab758445b2d16b51ed42458f973
matyzatka <matous.zatka@seznam.cz>
2022-05-17 16:46:13 +0200

    working

---
+++ main.js
@@ -0,0 +1,133 @@
+let data1 = {
+    photo: 'https://i.ibb.co/Rgc1kSC/Dali.jpg',
+    title: 'Salvador Dalí: Paintings',
+    description: 'Welcome to my little gallery, dedicated to the one of the most interesting and controversial artists. Salvador Dalí is among the most versatile and prolific artists of the 20th century and the most famous Surrealist. Though chiefly remembered for his painterly output, in the course of his long career he successfully turned to sculpture, printmaking, fashion, advertising, writing, and, perhaps most famously, filmmaking in his collaborations with Luis Buñuel and Alfred Hitchcock. Dalí was renowned for his flamboyant personality and role of mischievous provocateur as much as for his undeniable technical virtuosity. In his early use of organic morphology, his work bears the stamp of fellow Spaniards Pablo Picasso and Joan Miró. His paintings also evince a fascination for Classical and Renaissance art, clearly visible through his hyper-realistic style and religious symbolism of his later work.'
+};
+
+let data2 = {
+    photo: 'https://i.ibb.co/Nsq3hwk/2.jpg',
+    title: 'The Temptation of Saint Anthony (1946)',
+    description: 'Artists and authors have long represented the temptation of St. Anthony in their art. The Temptation of St. Anthony is painted with oil on canvas. It depicts a desert-like landscape: a low horizon line with high clouds and dark, warm tones in an azure sky. The figure of St. Anthony kneels in the bottom left corner. He holds up a cross in his right hand and with his left hand supports himself on an ambiguous form. A human skull lies by his right foot. A parade of elephants led by a horse approach St. Anthony. The elephants carry symbolic objects representing temptation: a statue of a nude woman holding her breasts, an obelisk, a building complex confining a nude, disembodied female torso, and a vertical tower. The animals have exaggerated, long, spindly legs, making them appear weightless.'
+};
+
+let data3 = {
+    photo: 'https://i.ibb.co/2hvmnWM/3.jpg',
+    title: 'The Great Masturbator (1929)',
+    description: 'This is one of the most famous Salvador Dalís surrealist paintings, and this art mainly represents the artist’s fear of female sexuality and castration. Nevertheless, it also highlights the potential to attain sexual and spiritual liberation. The most famous element in this painting is the grasshopper, which depicts Dali’s fear of insects. Currently displayed in Museo Nacional Centro de Arte Reina Sofía, Madrid, this painting is still considered one of his most notable works.'
+};
+
+let data4 = {
+    photo: 'https://i.ibb.co/f8RwDDs/4.jpg',
+    title: 'Swans reflecting elephants (1937)',
+    description: 'This oil on canvas was one of the more easily recognizable surrealism paintings by Salvador Dali. Taking reference from Sigmund Freud’s studies, Dali would self-induce a state of paranoia, allowing his brain to create any unseen links between two objects. This is what he referred to as his Paranoic-critical period. This painting is a product of that. However, you will not see it anymore since it is a part of a private collection now.'
+};
+
+let data5 = {
+    photo: 'https://i.ibb.co/y4HJRrN/1.jpg',
+    title: 'The Persistence of Memory (1931)',
+    description: 'The Persistence of Memory, also known as Soft Watches or Melting Clocks, is one of Dalí’s most famous pieces. Several of his favorite recurring images are present in this work. The setting is one he often used: the seashore of Catalonia at Cape Creus. His melting-clock imagery mocks the rigidity of chronometric time. The watches themselves look like soft cheese—indeed, by Dalí’s own account they were inspired by hallucinations after eating Camembert cheese. (He used a process he called the “paranoiac critical method,”: he deliberately provoked hallucinations as a path to his own subsconcious.)'
+};
+
+let data6 = {
+    photo: 'https://i.ibb.co/CW6PpCy/6.jpg',
+    title: 'The Disintegration of the Persistence of Memory (1954)',
+    description: 'This was painted as a response to the previously mentioned Surrealism Salvador painting, The Persistence of Memory. This can be called a recreation of that painting, as there are many of the same elements. However, this painting marked how Dali lost his interest in surrealism and became intrigued by physics, mainly the quantum world. You can now find this painting in St. Peter, Florida, in the Salvador Dali Museum.'
+};
+
+let data7 = {
+    photo: 'https://i.ibb.co/9Vm9wts/7.jpg',
+    title: 'Spider of the Evening (1940)',
+    description: 'This was more of a violent continuation of The Persistence of Memory and famously included melting clocks. By this Surrealism art, Dali tried to show the omnipresence of time and how it masters the habits of a human being. It is said that the inspiration arises from a surreal interpretation of how the cheese melts in the sun.'
+};
+
+let data8 = {
+    photo: 'https://i.ibb.co/m59LXQn/8.jpg',
+    title: 'Galatea of the Spheres (1952)',
+    description: 'This painting is the perfect example of how Salvador Dali mimicked the aesthetical structure of DNA. In this painting, he parodied the molecular structure of human beings. This mainly represents his newfound interest in science and theories relating to the disintegration of an atom.'
+};
+
+let data9 = {
+    photo: 'https://i.ibb.co/804fzy8/9.jpg',
+    title: 'Dream Caused by the Flight of a Bee around a Pomegranate a Second Before Awakening (1944)',
+    description: 'More commonly known as the Dream Cause by the Flight of a Bee, the woman in this painting is said to be his wife, Gala. The pomegranate in this painting is a Christian symbol of resurrection and fertility, and the bee above that symbolizes the Virgin. The distant seascape is also a much-argued symbol by art enthusiasts and critics. You can now find this painting in the Thyssen-Bornemisza Museum, located in Madrid.'
+};
+
+let data10 = {
+    photo: 'https://i.ibb.co/VD5NM6L/10.jpg',
+    title: 'Christ of Saint John of the Cross (1951)',
+    description: 'A drawing mainly inspired this painting of Dali. He was tripping on ecstasy, a common drug found with Dali, while he had this vision of Christ. It depicts Jesus Christ on the cross, with a dark sky as the background floating over water with fishermen and a boat. This is currently presented as the collection of Kelvin grove Art Gallery and Museum, Glasgow. This is the perfect example of Salvador Dali’s surrealism.'
+};
+
+let data11 = {
+    photo: 'https://i.ibb.co/YhM52v2/11.jpg',
+    title: 'Soft Construction with Boiled Beans (Premonition of Civil War) (1936)',
+    description: 'Salvador Dali created this piece to depict the horrors faced during the Spanish Civil Wars. However, the interesting fact about this painting is that it was painted only 6 months prior to the actual war. He later said that he was aware of a war beginning. Located in the Philadelphia Museum of Art, this is a must-visit if you are a fan of Dali’s work.'
+};
+
+let data12 = {
+    photo: 'https://i.ibb.co/LpnTH7v/12.jpg',
+    title: 'The Ghost of Vermeer of Delft Which Can Be Used As a Table (1934)',
+    description: 'When compared to his other paintings, this was a rather small oil painting. It takes inspiration from The Art of Painting, which was done by a seventeenth-century painter. It was Dali’s take on this self-portrait of Vermeer. This is one of Dali’s several compositions showcasing his huge admiration for Vermeer with his back to us. This painting is located with The Disintegration of the Persistence of Memory in the Salvador Dalí Museum in Florida.'
+};
+
+let data13 = {
+    photo: 'https://i.ibb.co/b3yGQvC/13.jpg',
+    title: 'The Burning Giraffe (1937)',
+    description: 'First used by Dali in his 1930 film L’Age d’Or, which loosely translates to The Golden Age, the burning giraffe was again spotted in a painting called The Invention of Monsters. This Salvador Dali surrealism artwork could be described as a masculine apocalyptic monster that was also warming to the effects and possibilities of war. He believed the only way humanity could be saved through psychoanalysis.'
+};
+
+let data14 = {
+    photo: 'https://i.ibb.co/0twCTWf/14.jpg',
+    title: 'Metamorphosis of Narcissus (1937)',
+    description: 'Dali was undoubtedly one of the biggest fans of Sigmund Freud’s work. During their meeting, he thought of bringing this painting along to discuss the psychoanalytical theory of Narcissism and the concept of paranoia. On seeing this painting, he was also permitted to sketch Freud himself.'
+};
+
+let data15 = {
+    photo: 'https://i.ibb.co/kXT9BB0/15.jpg',
+    title: 'The Elephants (1948)',
+    description: 'Dali’s impression of elephants is usually portrayed with multi-jointed, long, and rather invisible legs of desires, carrying loads on their backs. These animals are used to represent the future and also as a symbol of strength. Most of Dali’s paintings show elephants carrying obelisks. These are considered to be a symbol of domination and power in Salvador Dalís surrealism.'
+};
+
+let currentPhoto = 0;
+
+let imagesData = [data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15];
+
+$('#photo').attr('src', imagesData[currentPhoto.photo]);
+
+
+let loadPhoto = (photoNumber) => {
+    $('#photo').attr('src', imagesData[photoNumber].photo);
+}
+
+loadPhoto(currentPhoto);
+
+$('#arrowRight').click(() => {
+    if (currentPhoto < 14) {
+        currentPhoto++;
+        loadPhoto(currentPhoto);
+    } else {
+        currentPhoto = 0;
+        loadPhoto(currentPhoto);
+    }
+})
+
+$('#arrowLeft').click(() => {
+    if (currentPhoto > 0) {
+        currentPhoto--;
+        loadPhoto(currentPhoto);
+    } else {
+        currentPhoto = 14;
+        loadPhoto(currentPhoto);
+    }
+})
+
+imagesData.forEach((item, index) => {
+    $('.thumbnails').append(`<img data-index="${index}" src="${item.photo}">`);
+    $('.thumbnails img').click((event) => {
+        let indexClicked = $(event.target).attr('data-index');
+        let numberIndex = parseInt(indexClicked);
+        console.log(numberIndex);
+        currentPhoto = numberIndex;
+        loadPhoto(currentPhoto);
+    });
+});
